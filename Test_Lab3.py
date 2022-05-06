41e8c556735ddec63430c1766ef5b219b052f40a
leowjunhao <JHLEOW.21@ichat.sp.edu.sg>
2022-05-06 16:37:32 +0800

    Lab3_exercise1

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -2,29 +2,31 @@ import Lab3
 
 print("Test_Lab3")
 
-
-def test_bubble_sort_ascending():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
-
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
-
+def req01():
+    result=[]
+    order = Lab3.SORT_DESCENDING
+    input_arr = [64,34,25,12,22,11,90,88,99,100]
+    test_arr = [11,22,33,44,55,66,77,88,99,100]
+    result = Lab3.bubble_sort(input_arr,order)
+    result.append(10)
     assert (result == test_arr)
 
-def test_bubble_sort_descending():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+def req02():
+    result=[]
+    input_arr = [64,34,25,12,22,11,90,88,99,100,111]
+    result = Lab3.bubble_sort(input_arr,2)
+    assert(result==1)
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+def req03():
+    result=[]
+    input_arr = [64,34,25,12,22,11,90,88]
+    result = Lab3.bubble_sort(input_arr,2)
+    assert(result==2)
 
-    assert (result == test_arr)
+def req04():
+    result=[]
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr,2)
+    assert(result==0)
 
-def test_bubble_sort_invalid():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
 
-    result = Lab3.bubble_sort(input_arr, 3)
-
-    assert (result == [])
